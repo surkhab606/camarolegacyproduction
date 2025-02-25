89a53bf6f62f4f9598600b37c19dfd3ae0dcb7a1
30189028 <surkhab.mundi@ucalgary.ca>
2025-02-25 13:31:32 -0700

    v1.06: Working on image implementation. Data file currently has 0 images because I intentionally left them out. I wanted to wait until now to see how an image would look so I could see what dimensions look best.

--- camaroproject/src/CamaroPage.jsx
+++ camaroproject/src/CamaroPage.jsx
@@ -3,9 +3,14 @@ import CamaroData from './data/CamaroData.js'
 import {useParams} from "react-router-dom";
 
 function CamaroPage() {
-    const { year } = useParams();
-    const intYear = Number(year);
-    CamaroData.find((camaro) => camaro.year === intYear);
+        const { year } = useParams();
+        const intYear = Number(year);
+        const camaro = CamaroData.find((camaro) => camaro.year === intYear);
+        return (
+            <div>
+                {camaro ? <div className="camaroimage">{camaro.image}</div> : <p>Camaro Not Found</p>}
+            </div>
+        )
 }
 
 export default CamaroPage
