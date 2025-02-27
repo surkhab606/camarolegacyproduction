de789f183844e183984b81b2a02d795f95ab4443
30189028 <surkhab.mundi@ucalgary.ca>
2025-02-26 20:27:19 -0700

    v1.14: Removed console log for image path. Realized that our scroll calculations are useless... right now the pages are just a glorified image gallery, there is nothing to scroll past the image to. Going to add a clean gradient effect into a longer page.

--- camaroproject/src/CamaroPage.jsx
+++ camaroproject/src/CamaroPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import CamaroData from './data/CamaroData.js'
 import {useParams} from "react-router-dom";
 
@@ -6,12 +6,27 @@ function CamaroPage() {
         const { year } = useParams();
         const intYear = Number(year);
         const camaro = CamaroData.find((camaro) => camaro.year === intYear);
-        console.log("Image Path:", `/assets/${camaro.image}`);
-        const windowScroll = (window.scrollY / (document.body.scrollHeight - window.innerHeight));
+        const [scrollPercent, setScrollPercent] = useState(0);
+
+    {/*Determine scroll percentage on page*/}
+    useEffect(() => {
+        const handleScroll = () => {
+            const percent = (window.scrollY / (document.body.scrollHeight - window.innerHeight)) * 100;
+            setScrollPercent(percent);
+        };
+
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+        console.log(scrollPercent);
 
     return (
                 <div>
+                    {/*Dynamic Camaro Image*/}
                     <div className ="camaro-background" style={{ backgroundImage: `url(/assets/${camaro.image})`}}></div>
+
+                    {/*Camaro Info Div*/}
+                    <div className ="camaro-overlay" style={{ width: `${scrollPercent}%`, transition: "width 0.2s ease-out"}}></div>
                 </div>
         );
 
