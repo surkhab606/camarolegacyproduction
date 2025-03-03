9dc6e0d7882f43f5e283288674369b380dce753e
30189028 <surkhab.mundi@ucalgary.ca>
2025-03-02 21:56:48 -0700

    v1.15: Fell into a trap of using GPT to copy rather than learn. It's the greatest teacher but it can also spell my downfall, I can't let my impulses get out of control and just copy paste my way into a finished product. This means something to me. This needs to be done by me.

--- camaroproject/src/CamaroPage.jsx
+++ camaroproject/src/CamaroPage.jsx
@@ -6,27 +6,13 @@ function CamaroPage() {
         const { year } = useParams();
         const intYear = Number(year);
         const camaro = CamaroData.find((camaro) => camaro.year === intYear);
-        const [scrollPercent, setScrollPercent] = useState(0);
 
-    {/*Determine scroll percentage on page*/}
-    useEffect(() => {
-        const handleScroll = () => {
-            const percent = (window.scrollY / (document.body.scrollHeight - window.innerHeight)) * 100;
-            setScrollPercent(percent);
-        };
-
-        window.addEventListener("scroll", handleScroll);
-        return () => window.removeEventListener("scroll", handleScroll);
-    }, []);
-        console.log(scrollPercent);
-
-    return (
+        return (
                 <div>
                     {/*Dynamic Camaro Image*/}
                     <div className ="camaro-background" style={{ backgroundImage: `url(/assets/${camaro.image})`}}></div>
 
-                    {/*Camaro Info Div*/}
-                    <div className ="camaro-overlay" style={{ width: `${scrollPercent}%`, transition: "width 0.2s ease-out"}}></div>
+
                 </div>
         );
 
