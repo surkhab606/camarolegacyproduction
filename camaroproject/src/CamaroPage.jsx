e46a4ec3dac2c4afb2134940048059ba86c7791f
30189028 <surkhab.mundi@ucalgary.ca>
2025-03-02 23:46:35 -0700

    v1.22: Creating animation for smooth fade in effect for year text. using the useeffect hook!

--- camaroproject/src/CamaroPage.jsx
+++ camaroproject/src/CamaroPage.jsx
@@ -1,10 +1,18 @@
 import CamaroData from './data/CamaroData.js'
 import {useParams} from "react-router-dom";
+import {useEffect, useState} from "react";
 
 function CamaroPage() {
         const { year } = useParams();
         const intYear = Number(year);
         const camaro = CamaroData.find((camaro) => camaro.year === intYear);
+        const [yearOpacity, setYearOpacity] = useState(false);
+
+        useEffect(() => {
+            setTimeout(() => {
+                setYearOpacity(true);
+            }, 1000);
+        }, []);
 
         return (
                 <div>
